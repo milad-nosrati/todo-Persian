e2b1d6aef6579ef6e7b68e7ba0a76ca779a100f8
Milad Nosrati <53852333+milad-nosrati@users.noreply.github.com>
2021-01-22 00:20:35 +1100

    Add files via upload

---
+++ build/precache-manifest.938a9ff5355a4b88522bb23665752ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c302942d1c80baa513b1c7ff9daae2ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f68afca22765045aa19",
+    "url": "/static/css/2.6862be43.chunk.css"
+  },
+  {
+    "revision": "1f68afca22765045aa19",
+    "url": "/static/js/2.3719da49.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.3719da49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e521ceab22b0860d59",
+    "url": "/static/js/main.2ce1050e.chunk.js"
+  },
+  {
+    "revision": "ddd5be31d21629b3845f",
+    "url": "/static/js/runtime-main.1f5a0244.js"
+  }
+]);
